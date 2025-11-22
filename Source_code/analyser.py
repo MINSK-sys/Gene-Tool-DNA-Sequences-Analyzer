1a42bce20cd6dab98500c09461027e55dbe8596e
MINSK-sys <minakshi.25boe10108@vitbhopal.ac.in>
2025-11-22 17:51:00 +0530

    Implement GC content calculation function
    Add function to calculate GC content in DNA sequences.

--- Source_code/analyser.py
+++ Source_code/analyser.py
@@ -1,1 +1,17 @@
+# File: genetics/analyzer.py
 
+def calculate_gc_content(sequence):
+    """
+    Calculates the percentage of G and C bases in a DNA sequence.
+    """
+    seq = sequence.upper()
+    total_bases = len(seq)
+    
+    if total_bases == 0:
+        return 0.0
+    
+    g_count = seq.count('G')
+    c_count = seq.count('C')
+    
+    gc_percent = ((g_count + c_count) / total_bases) * 100
+    return round(gc_percent, 2)
